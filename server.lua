ee3f5b612186896878c248d2608e2f021267541d
dodiban <69380454+dodibanScripts@users.noreply.github.com>
2024-09-23 11:19:45 -0300

    Update server.lua
    VORP CORE VERSION ON SERVER

--- server.lua
+++ server.lua
@@ -5,6 +5,8 @@ local config = exports['dodi_flags']:getConfig()
 ------------<items>---------
 ----------------------------
 
+------------- REDEM RP --------------------
+
 -- Bandeira Pirate
 RegisterServerEvent("RegisterUsableItem:bandeira_pirate")
 TriggerEvent("redemrp:registerUsableItem", "bandeira_pirate", function(source)
@@ -490,4 +492,457 @@ TriggerEvent("redemrp:registerUsableItem", "bandeira_whitelongflag", function(so
         TriggerClientEvent('dodi_flags:use:whitelongflag', _source)
     end)
 end)
+
+
+
+
+------------- VORPCORE --------------------
+
+local config = exports['dodi_flags']:getConfig()
+
+-- local VorpCore = {}
+-- TriggerEvent("getCore",function(core)
+--     VorpCore = core
+-- end)
+
+-- VorpInv = exports.vorp_inventory:vorp_inventoryApi()
+
+
+-- ----------------------------
+-- ------------<items>---------
+-- ----------------------------
+
+-- -- Bandeira Pirate
+-- VorpInv.RegisterUsableItem("bandeira_pirate", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:pirate', _source)
+-- end)
+
+-- -- Bandeira American
+-- VorpInv.RegisterUsableItem("bandeira_american", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:american', _source)
+-- end)
+
+-- -- Bandeira Mexican
+-- VorpInv.RegisterUsableItem("bandeira_mexican", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:mexican', _source)
+-- end)
+
+-- -- Bandeira Canada
+-- VorpInv.RegisterUsableItem("bandeira_canada", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:canada', _source)
+-- end)
+
+-- -- Bandeira LGBTQ
+-- VorpInv.RegisterUsableItem("bandeira_lgbtq", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:lgbtq', _source)
+-- end)
+
+-- -- Bandeira Gypsy
+-- VorpInv.RegisterUsableItem("bandeira_gypsy", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:gypsy', _source)
+-- end)
+
+-- -- Bandeira Irish
+-- VorpInv.RegisterUsableItem("bandeira_irish", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:irish', _source)
+-- end)
+
+-- -- Bandeira UK
+-- VorpInv.RegisterUsableItem("bandeira_uk", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:uk', _source)
+-- end)
+
+-- -- Bandeira BR
+-- VorpInv.RegisterUsableItem("bandeira_br", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:br', _source)
+-- end)
+
+-- -- Bandeira FR
+-- VorpInv.RegisterUsableItem("bandeira_fr", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:fr', _source)
+-- end)
+
+-- -- Bandeira IT
+-- VorpInv.RegisterUsableItem("bandeira_it", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:it', _source)
+-- end)
+
+-- -- Bandeira JM
+-- VorpInv.RegisterUsableItem("bandeira_jm", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:jm', _source)
+-- end)
+
+-- -- Bandeira GER
+-- VorpInv.RegisterUsableItem("bandeira_ger", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:ger', _source)
+-- end)
+
+-- -- Bandeira CONF
+-- VorpInv.RegisterUsableItem("bandeira_conf", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:conf', _source)
+-- end)
+
+-- -- Bandeira UNION
+-- VorpInv.RegisterUsableItem("bandeira_union", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:union', _source)
+-- end)
+
+-- -- Bandeira Lemoyne
+-- VorpInv.RegisterUsableItem("bandeira_lemoyne_1", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:lemoyne_1', _source)
+-- end)
+
+-- -- Bandeira New Hannover
+-- VorpInv.RegisterUsableItem("bandeira_newhannover_1", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:newhannover_1', _source)
+-- end)
+
+-- -- Bandeira West Elizabeth
+-- VorpInv.RegisterUsableItem("bandeira_westelizabeth_1", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:westelizabeth_1', _source)
+-- end)
+
+-- -- Bandeira New Austin
+-- VorpInv.RegisterUsableItem("bandeira_newaustin_1", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:newaustin_1', _source)
+-- end)
+
+-- -- Bandeira Ambarino
+-- VorpInv.RegisterUsableItem("bandeira_ambarino_1", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:ambarino_1', _source)
+-- end)
+
+-- -- Bandeira Comanche
+-- VorpInv.RegisterUsableItem("bandeira_comanche", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:comanche', _source)
+-- end)
+
+-- -- Bandeira Navajo
+-- VorpInv.RegisterUsableItem("bandeira_navajo", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:navajo', _source)
+-- end)
+
+-- -- Bandeira Lakota
+-- VorpInv.RegisterUsableItem("bandeira_lakota", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:lakota', _source)
+-- end)
+
+-- -- Bandeira Inuit
+-- VorpInv.RegisterUsableItem("bandeira_inuit", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:inuit', _source)
+-- end)
+
+-- -- Bandeira Australia
+-- VorpInv.RegisterUsableItem("bandeira_australia", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:australia', _source)
+-- end)
+
+-- -- Bandeira Belgium
+-- VorpInv.RegisterUsableItem("bandeira_belgium", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:belgium', _source)
+-- end)
+
+-- -- Bandeira Catalonia
+-- VorpInv.RegisterUsableItem("bandeira_catalonia", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:catalonia', _source)
+-- end)
+
+-- -- Bandeira China
+-- VorpInv.RegisterUsableItem("bandeira_china", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:china', _source)
+-- end)
+
+-- -- Bandeira Cuba
+-- VorpInv.RegisterUsableItem("bandeira_cuba", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:cuba', _source)
+-- end)
+
+-- -- Bandeira Czech
+-- VorpInv.RegisterUsableItem("bandeira_czech", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:czech', _source)
+-- end)
+
+-- -- Bandeira Denmark
+-- VorpInv.RegisterUsableItem("bandeira_denmark", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:denmark', _source)
+-- end)
+
+-- -- Bandeira Finland
+-- VorpInv.RegisterUsableItem("bandeira_finland", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:finland', _source)
+-- end)
+
+-- -- Bandeira Guarma
+-- VorpInv.RegisterUsableItem("bandeira_guarma", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:guarma', _source)
+-- end)
+
+-- -- Bandeira Hungary
+-- VorpInv.RegisterUsableItem("bandeira_hungary", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:hungary', _source)
+-- end)
+
+-- -- Bandeira Kuwait
+-- VorpInv.RegisterUsableItem("bandeira_kuwait", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:kuwait', _source)
+-- end)
+
+-- -- Bandeira Flandres
+-- VorpInv.RegisterUsableItem("bandeira_flandres", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:flandres', _source)
+-- end)
+
+-- -- Bandeira New Zealand
+-- VorpInv.RegisterUsableItem("bandeira_newzealand", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:newzealand', _source)
+-- end)
+
+-- -- Bandeira Philippines
+-- VorpInv.RegisterUsableItem("bandeira_philippines", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:philippines', _source)
+-- end)
+
+-- -- Bandeira Poland
+-- VorpInv.RegisterUsableItem("bandeira_poland", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:poland', _source)
+-- end)
+
+-- -- Bandeira Portugal
+-- VorpInv.RegisterUsableItem("bandeira_portugal", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:portugal', _source)
+-- end)
+
+-- -- Bandeira Puerto Rico
+-- VorpInv.RegisterUsableItem("bandeira_puertorico", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:puertorico', _source)
+-- end)
+
+-- -- Bandeira Russia
+-- VorpInv.RegisterUsableItem("bandeira_russia", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:russia', _source)
+-- end)
+
+-- -- Bandeira Saudi
+-- VorpInv.RegisterUsableItem("bandeira_saudi", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:saudi', _source)
+-- end)
+
+-- -- Bandeira Serbia
+-- VorpInv.RegisterUsableItem("bandeira_serbia", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:serbia', _source)
+-- end)
+
+-- -- Bandeira Spain
+-- VorpInv.RegisterUsableItem("bandeira_spain", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:spain', _source)
+-- end)
+
+-- -- Bandeira Sweden
+-- VorpInv.RegisterUsableItem("bandeira_sweden", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:sweden', _source)
+-- end)
+
+-- -- Bandeira Thailand
+-- VorpInv.RegisterUsableItem("bandeira_thailand", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:thailand', _source)
+-- end)
+
+-- -- Bandeira Turkey
+-- VorpInv.RegisterUsableItem("bandeira_turkey", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:turkey', _source)
+-- end)
+
+-- -- Bandeira Ukraine
+-- VorpInv.RegisterUsableItem("bandeira_ukraine", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:ukraine', _source)
+-- end)
+
+-- -- Bandeira White
+-- VorpInv.RegisterUsableItem("bandeira_white", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:white', _source)
+-- end)
+
+-- -- Bandeira Red
+-- VorpInv.RegisterUsableItem("bandeira_red", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:red', _source)
+-- end)
+
+-- -- Bandeira Camp Flag
+-- VorpInv.RegisterUsableItem("bandeira_campflag", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:campflag', _source)
+-- end)
+
+-- -- Bandeira Lemoyne Long
+-- VorpInv.RegisterUsableItem("bandeira_lemoynelongflag", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:lemoynelongflag', _source)
+-- end)
+
+-- -- Bandeira White Long
+-- VorpInv.RegisterUsableItem("bandeira_whitelongflag", function(data)
+--     local _source = data.source
+--     local User = VorpCore.getUser(_source)
+--     local Character = User.getUsedCharacter
+--     TriggerClientEvent('dodi_flags:use:whitelongflag', _source)
+-- end)
+
+
 
